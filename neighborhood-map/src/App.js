d8ea3924c2e5ec4ce82d77cc0320ed8828f7d250
Kevin Zhu <kevin.z1989@gmail.com>
2018-12-01 00:00:12 -0500

    neighborhoodmap redo

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -1,141 +1,126 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import axios from 'axios';
 import SideBar from './sidebar';
 import './App.css';
 
-
 class App extends Component {
 
   state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
-    locations: [],
-    loadingError: false,
-    errorMsg: ''
-  };
+    restaurants: [],
+    map: null,
+    markers: [],
+    infoWindows: [],
+  }
 
-  locations;
+  restaurants;
 
   componentDidMount() {
-    this.getLocations()
+    window.initMap = this.initMap;
+    this.getLocations();
   }
-  
 
-  //gets location from foursquare api
   getLocations = () => {
-  const api = 'https://api.foursquare.com/v2/venues/explore?';
-  const parameters = new URLSearchParams({
-    client_id: 'RMSS5KJD0JNRDQLDORWATPTX503FEYT2P4U5VXNXJKS2VDYJ',
-    client_secret: '5QKMF1W1TLQVQSJ1SKQDXTM2FMWMYSM1NK45KNOH2CFP5NRX',
-    query: 'restaurant',
-    near: 'New York',
-    v: '20182507'
-  });
-
-  axios.get(`${api}${parameters}` )
-    .then(
-      res => {
-        this.locations = res.data.response.groups[0].items;
-        this.setState({
-          locations: res.data.response.groups[0].items
-        });
-      },
-      err => {
-       this.setState({
-        loadingError: true,
-        errorMsg: err
-       });
-      },
-    );
-  }
-
-  //display info window
-  onMarkerClick = (props, e) => {
-    this.setState({
-      markerLocation: props.position,
-      showingInfoWindow: true,
-      name: props.name
+    const api = 'https://api.foursquare.com/v2/venues/explore?';
+    const parameters = new URLSearchParams({
+      client_id: 'RMSS5KJD0JNRDQLDORWATPTX503FEYT2P4U5VXNXJKS2VDYJ',
+      client_secret: '5QKMF1W1TLQVQSJ1SKQDXTM2FMWMYSM1NK45KNOH2CFP5NRX',
+      query: 'restaurant',
+      near: 'New York',
+      v: '20182507'
     });
-  }
-
-  //used to filter locations
+  
+    axios.get(`${api}${parameters}` )
+      .then(
+        res => {
+          this.restaurants = res.data.response.groups[0].items;
+          this.setState({
+            restaurants: res.data.response.groups[0].items
+          }, loadScript());
+        },
+        err => {
+         this.setState({
+          loadingError: true,
+          errorMsg: err
+         });
+        },
+      );
+  } 
+  
   updateFilter = (event) => {
     this.setState({
-      locations: this.locations.filter(location => location.venue.name.toLowerCase().includes(event.target.value))
+      restaurants: this.restaurants.filter(restaurant => restaurant.venue.name.toLowerCase().includes(event.target.value))
     });
+            
+    this.state.markers.forEach(
+      marker => {marker.setMap(null)}
+    );
+    this.state.markers = [];
+    this.state.infoWindows = [];
+    this.generateMarkerAndInfoWindow(this.state.map);
   }
   
+  initMap = () => {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7408666, lng: -73.9879822},
+      zoom: 14
+    });
+    this.setState({map});
+    this.generateMarkerAndInfoWindow(map);
+  }
 
-  //resets info window and activemarker
-  onMapClicked = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-      })
-    }
-  };
-
-  componentDidCatch(error, info) {
-    // Display fallback UI
-    this.setState({ 
-      loadingError: true,
-      errorMsg: info
+  generateMarkerAndInfoWindow(map) {
+    this.state.restaurants.map(restaurant => {
+      let contentString = `${restaurant.venue.name}`;
+      let infowindow = new window.google.maps.InfoWindow({
+        name:restaurant.venue.name,
+        content: contentString
+      });
+      let marker = new window.google.maps.Marker({
+        position: {lat: restaurant.venue.location.lat , lng: restaurant.venue.location.lng},
+        animation: window.google.maps.Animation.DROP,
+        map: map,
+        title: restaurant.venue.name
+      });
+      this.state.infoWindows.push(infowindow);
+      this.state.markers.push(marker);
+      marker.addListener('click', function() {
+        infowindow.open(map, marker)
+      });
+      
+      
     });
   }
 
   render() {
-    const displayLocations = this.state.locations.map(
-      location => {
-        const description = `<div>${location.venue.name}</div>`
-        return (
-        <Marker 
-          key={location.venue.id}
-          onClick={this.onMarkerClick}
-          title={location.venue.categories[0].name}
-          name= {location.venue.name}
-          position={{
-            lat: location.venue.location.lat,
-            lng: location.venue.location.lng
-          }}
-        >
-        </Marker>
-      )}
-    );
-    
-    if(this.state.apiError) {
+    if (this.state.loadingError) {
       return (
         <div>
-          <h1>Sorry, could not load locations from api.</h1>
+          <h4>Error loading locations {this.state.errorMsg}</h4>
         </div>
       )
     }
     return (
       <div>
-        <SideBar locations={displayLocations} updateFilter={this.updateFilter}/>
-        <Map 
-          google={this.props.google} 
-          onClick={this.onMapClicked} 
-          initialCenter={{
-            lat: 40.75063,
-            lng: -73.9941115
-          }}
-        >
-          {displayLocations}
-          <InfoWindow
-            position={this.state.markerLocation}
-            visible={this.state.showingInfoWindow}>
-              <div>
-                <h1>{this.state.name}</h1>
-              </div>
-          </InfoWindow>
-        </Map>
+        <SideBar 
+          restaurants={this.state.restaurants} 
+          markers={this.state.markers} 
+          infoWindows={this.state.infoWindows}
+          updateFilter={this.updateFilter}
+          map={this.state.map}
+        />
+        <div id='map'></div>
       </div>
     )
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyB1MDOJ_HnMOjWnnZUV0Vw8EBBYc-4FCGY')
-})(App);
+function loadScript() {
+  let index = window.document.getElementsByTagName('script')[0];
+  let script = window.document.createElement('script');
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB1MDOJ_HnMOjWnnZUV0Vw8EBBYc-4FCGY&callback=initMap';
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
+}
+
+export default App;
--- neighborhood-map/src/sidebar.js
+++ neighborhood-map/src/sidebar.js
@@ -4,41 +4,61 @@ import {slide as Menu } from 'react-burger-menu';
 class SideBar extends Component {
 
     state = {
-        locations: [],
+        restaurants: [],
         filter: '',
-        displayLocations: []
+        displayLocations: [],
+        markers: [],
+        infoWindows: [],
+        map: null
     }
 
     componentWillReceiveProps(props){
         this.setState({
-            locations: props.locations,
+            restaurants: props.restaurants,
             updateFilter: props.updateFilter,
-            displayLocations: props.displayLocations
+            markers: props.markers,
+            infoWindows: props.infoWindows,
+            map: props.map
         });
-        
+        console.log(props);
+        console.log('this state', this.state);
     }
 
-    makeMarker(locationInfo) {
-        locationInfo.onClick(locationInfo);
+    openMarker(name) {
+        console.log(this.state.markers);
+        this.state.markers.forEach(
+            marker => {
+                // console.log('marker', marker)
+                if (name === marker.title) {
+                    this.state.infoWindows.forEach(
+                        infoWindow => {
+                            if (marker.title === infoWindow.name) {
+                                infoWindow.open(this.state.map, marker);
+                            }
+                        }
+                    )
+                }
+            }
+        )
     }
 
     render(){
-        const displayLocation =  this.state.locations.map(
-                (location) => (
-                    <li key={location.key} aria-label='location name' onClick={this.makeMarker.bind(this, location.props)}>
-                        {location.props.name}
-                    </li>
-                )
-            );
+        const displayLocation = this.state.restaurants.map(
+            restaurant => (
+                <li aria-label='location name' key={restaurant.referralId} onClick={() => {this.openMarker(restaurant.venue.name)}} value={restaurant.venue.name}>
+                    {restaurant.venue.name}
+                </li>
+            )
+        )
         return (
             <Menu>
                 <form>
                     <label>
                         <h3>Search</h3>
                         <input type='text' name='searchLocation' onChange={this.props? this.props.updateFilter : 0}/>
-                        {/* <ol> */}
+                        <ol>
                             {displayLocation}
-                        {/* </ol> */}
+                        </ol>
                     </label>
                 </form>
 
